371a1886a2d48263dce4048c73792a5cb5dc4e70
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-20 20:42:24 +0900

    static setting changed

--- deep_server_side/settings.py
+++ deep_server_side/settings.py
@@ -125,5 +125,5 @@ STATIC_URL = '/static/'
 # ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
-STATIC_ROOT = "/home/ubuntu/static_root/"
+STATIC_ROOT = "/home/ubuntu/static/"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
