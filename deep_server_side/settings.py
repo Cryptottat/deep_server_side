47d5747e1f3cc8469c91ec1e29777fb5b9f5b53f
morgan-b-kim <morgan-b-kim@gmail.com>
2023-04-20 21:01:09 +0900

    change settings.py BASE_DIR

--- deep_server_side/settings.py
+++ deep_server_side/settings.py
@@ -13,8 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = "/home/ubuntu/"
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,7 +124,7 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 STATIC_URL = '/static_root/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static_files",
 # ]
